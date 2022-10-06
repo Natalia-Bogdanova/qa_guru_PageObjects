6651e8177c8fd258892f4bc4eed2abd591286df1
Natalia-Bogdanova <bogdanowa.vlg@gmail.com>
2022-10-06 16:12:58 +0300

    add new MiniTest

--- src/test/java/test/testPageObjectsTestForm.java
+++ src/test/java/test/testPageObjectsTestForm.java
@@ -1,4 +1,4 @@
-package com.bogdanova;
+package test;
 
         import com.codeborne.selenide.Condition;
         import com.codeborne.selenide.Configuration;
@@ -56,6 +56,44 @@ public class testPageObjectsTestForm {
                 text("3.jpg"),
                 text("red light, Geeta Colony"),
                 text("Haryana Panipat"));
+        $("#closeLargeModal").click();
+    }
+
+    @Test
+    void fillFormMiniTest() {
+        open("/automation-practice-form");
+        Selenide.zoom(0.8);
+        $("#firstName").setValue("Vadim");
+        $("#lastName").setValue("Pirogkov");
+        //$("#userEmail").setValue("Pirogkov@gmail.com");
+        $("#gender-radio-1").parent().click();
+        $("#userNumber").setValue("9051112233");
+        //$("#dateOfBirthInput").click();
+        //$(".react-datepicker__month-select").selectOption("June");
+        //$(".react-datepicker__year-select").selectOption("1951");
+        //$(".react-datepicker__day--016:not(.react-datepicker__day--selected").click();
+        //$("#subjectsInput").setValue("Maths").pressEnter();
+        //$("#hobbies-checkbox-2").parent().click();
+        //$("#uploadPicture").uploadFromClasspath("3.jpg");
+        //$("#currentAddress").setValue("red light, Geeta Colony");
+        //$(byText("Select State")).click();
+        //$(byText("Haryana")).click();
+        //$(byText("Select City")).click();
+        //$(byText("Panipat")).click();
+
+        $("#submit").click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(text("Vadim Pirogkov"),
+                //text("Pirogkov@gmail.com"),
+                text("Male"),
+                text("9051112233"));
+                //text("16 June,1951"),
+                //text("Maths"),
+                //text("Reading"),
+                //text("3.jpg"),
+                //text("red light, Geeta Colony"),
+                //text("Haryana Panipat"));
         $("#closeLargeModal").click();
     }
 }
